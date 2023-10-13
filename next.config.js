7a5af2bd87c4c9846621c1b1c21c940297c7fd24
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-10-13 16:46:25 +0200

    change the user image to google image

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ["lh3.googleuserconnect.com"],
+    domains: ["lh3.googleusercontent.com"],
   },
   webpack(config) {
     config.experiments = {
